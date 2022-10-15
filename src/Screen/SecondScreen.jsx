6bbac890b1ff52e2ff5746c0655aff8843fd33c4
Delanoor Nahm <hwn0809@gmail.com>
2022-10-15 17:43:21 +0900

    update second screen

--- src/Screen/SecondScreen.jsx
+++ src/Screen/SecondScreen.jsx
@@ -1,12 +1,16 @@
 import "../styles/second-screen.scss";
 import streetImg from "../images/street_black.webp";
+import barImg from "../images/street_bar.webp";
 import { useLayoutEffect, useRef } from "react";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 function SecondScreen() {
+  gsap.registerPlugin(ScrollTrigger);
   let container = useRef(null);
   let image = useRef(null);
+  let secondImage = useRef();
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
   const app = useRef();
@@ -16,7 +20,14 @@ function SecondScreen() {
     const ctx = gsap.context(() => {
       tl.current && tl.current.progress(0).kill();
       tl.current = gsap
-        .timeline()
+        .timeline({
+          scrollTrigger: {
+            trigger: ".container",
+            toggleActions: "resume none none pause",
+            // onEnter, onLeave, onEnterBack, onLeaveBack
+            // play, pause, resume, reverse, restart, reset, complete, none
+          },
+        })
         .to(imageReveal, {
           width: "0%",
           duration: 1.2,
@@ -32,12 +43,31 @@ function SecondScreen() {
           yPercent: 100,
           opacity: 0,
           duration: 0.3,
+        })
+
+        .to(".img-container", {
+          scale: 0.5,
+
+          yPercent: -20,
+          scrollTrigger: {
+            pin: true,
+            trigger: ".container",
+            start: "top 10%",
+            end: "+=100%",
+
+            scrub: true,
+          },
+        })
+        .to(".img-container-second", {
+          scale: 0.5,
+          scrollTrigger: {
+            trigger: ".container",
+            start: "top center",
+            end: "+=10%",
+
+            markers: true,
+          },
         });
-      // .to(".img-container", {
-      //   scale: 0.9,
-      //   xPercent: 10,
-      //   yPercent: -20,
-      // });
     }, app);
 
     return () => ctx.revert();
@@ -64,6 +94,19 @@ function SecondScreen() {
             excepturi maxime modi pariatur?
           </div>
         </>
+        <div className="second-container">
+          <div className="img-container-second" ref={secondImage}>
+            <img src={barImg} alt="street_bar" />
+          </div>
+          <div className="description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+            quasi praesentium dolores nostrum vero porro, nobis nihil esse saepe
+            iusto eligendi quisquam doloremque sint dolorem quo laborum et
+            aspernatur fugiat! Inventore quia ab aliquid temporibus laudantium
+            dolores tempore animi soluta sunt, et, molestiae unde ratione velit,
+            excepturi maxime modi pariatur?
+          </div>
+        </div>
       </div>
     </section>
   );
