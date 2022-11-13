7e818b50f0fc961383f53f2c06f43d8801b2e91b
hwn0809 <hwn0809@gmail.com>
2022-11-13 17:39:58 +0900

    scrolltrigger

--- src/Screen/SecondScreen.jsx
+++ src/Screen/SecondScreen.jsx
@@ -4,76 +4,114 @@ import barImg from "../images/street_bar.webp";
 import { useLayoutEffect, useRef } from "react";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
 import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function SecondScreen() {
-  gsap.registerPlugin(ScrollTrigger);
   let container = useRef(null);
-  let image = useRef(null);
+  let firstImage = useRef(null);
   let secondImage = useRef();
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
   const app = useRef();
   const tl = useRef();
+  const tlSecond = useRef();
 
+  // onEnter, onLeave, onEnterBack, onLeaveBack
+  // play, pause, resume, reverse, restart, reset, complete, none
   useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      tl.current && tl.current.progress(0).kill();
-      tl.current = gsap
-        .timeline({
-          scrollTrigger: {
-            // trigger: ".container",
-            toggleActions: "restart none none pause",
-            // onEnter, onLeave, onEnterBack, onLeaveBack
-            // play, pause, resume, reverse, restart, reset, complete, none
-          },
-        })
-        .to(imageReveal, {
-          width: "0%",
-          duration: 1.2,
-          ease: "power2.inOut",
-        })
-        .from(image, {
-          duration: 1.4,
-          scale: 1.6,
-          ease: "power2.inOut",
-          delay: -1.6,
-        })
-        .from(".img-title", {
-          yPercent: 100,
-          opacity: 0,
-          duration: 0.3,
-        })
+    let ctx = gsap.context(() => {
+      // tl.current && tl.current.progress(0).kill();
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container,
+          toggleActions: "restart none none pause",
+        },
+      });
+      tl.to(imageReveal, {
+        width: "0%",
+        duration: 1.2,
 
-        .to(".img-container", {
-          scale: 0.5,
+        ease: "power2.inOut",
+      });
+      tl.from(firstImage, {
+        duration: 1.4,
+        scale: 1.6,
+        ease: "power2.inOut",
+        delay: -1.6,
+      });
+      tl.from(".img-title", {
+        yPercent: 100,
+        opacity: 0,
+        duration: 0.3,
+      });
+      // tl.to(".img-container", {
+      //   scale: 0.5,
+      //   scrollTrigger: {
+      //     toggleActions: "restart none none pause",
+      //     pin: true,
+      //     trigger: ".container",
+      //     start: "top 10%",
+      //     end: "+=100%",
+      //     markers: true,
+      //     scrub: true,
+      //   },
+      // yPercent: -20,
+      // });
 
-          yPercent: -20,
-          scrollTrigger: {
-            toggleActions: "restart none none pause",
-            pin: true,
-            trigger: ".container",
-            start: "top 10%",
-            end: "+=100%",
+      // let secondTl = gsap.timeline();
 
-            scrub: true,
-          },
-        })
+      // secondTl.to(".img-container", {
+      //   scale: 0.6,
+      //   yPercent: -20,
+      //   scrollTrigger: {
+      //     trigger: ".second-container",
+      //     toggleActions: "restart none none pause",
+      //     immediateRender: false,
+      //     start: "top top",
+      //     end: "+=55%",
+      //     scrub: true,
+      //     pinSpacer: ".img-container",
+      //     markers: true,
+      // },
+      // });
 
-        .to(".img-container-second", {
+      gsap.utils.toArray(".img-container").forEach((img, i) => {
+        let imgAnimation = gsap.to(img, {
           scale: 0.6,
-          scrollTrigger: {
-            trigger: ".second-container",
-            start: "10% 10%",
-            end: "+=15%",
-            scrub: true,
-
-            markers: true,
-          },
+          yPercent: -20,
         });
+        ScrollTrigger.create({
+          trigger: img,
+          animation: imgAnimation,
+          start: "top 10%",
+          end: "+=100%",
+          pin: true,
+          pinSpacing: true,
+          markers: true,
+          scrub: true,
+        });
+      });
+      // gsap.to(".img-container", {
+      //   scale: 0.5,
+      //   scrollTrigger: {
+      //     toggleActions: "restart none none pause",
+      //     pin: true,
+      //     trigger: ".container",
+      //     start: "top 10%",
+      //     end: "+=100%",
+      //     // markers: true,
+      //     scrub: true,
+      //     pinSpacing: true,
+      //   },
+      //   yPercent: -20,
+      // });
     }, app);
 
-    return () => ctx.revert();
+    return () => {
+      ctx.revert();
+    };
   }, []);
 
   return (
@@ -83,7 +121,7 @@ function SecondScreen() {
           <h2 className="img-title">Great Photography</h2>
           <div className="img-container">
             <img
-              ref={(el) => (image = el)}
+              ref={(el) => (firstImage = el)}
               src={streetImg}
               alt="street_image"
             />
@@ -98,8 +136,12 @@ function SecondScreen() {
           </div>
         </>
         <div className="second-container">
-          <div className="img-container-second" ref={secondImage}>
-            <img src={barImg} alt="street_bar" />
+          <div className="img-container img-container-second">
+            <img
+              ref={(el) => (secondImage = el)}
+              src={barImg}
+              alt="street_bar"
+            />
           </div>
           <div className="description">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
